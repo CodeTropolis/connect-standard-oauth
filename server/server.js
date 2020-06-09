87b396645250a2effce2561ea750ffdbe92f7118
Michael <michael@codetropolis.com>
2020-06-08 22:08:51 -0500

    Removed Pino logger - going back to stock server.js.

--- server/server.js
+++ server/server.js
@@ -7,19 +7,9 @@ const session = require("express-session");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const uuidv4 = require('uuid').v4;
 
-// const pino = require('pino');
-// const expressPino = require('express-pino-logger');
-
-// const logger = pino({ level: process.env.LOG_LEVEL || 'info' });
-// Safe to use logger in prod??
-// const logger = pino({level:'info', prettyPrint: true});
-// const expressLogger = expressPino({ logger });
-
 const app = express();
 const port = process.env.PORT || 4242;
 
-
-// app.use(expressLogger);
 
 app.use(express.static(process.env.STATIC_DIR));
 
@@ -46,7 +36,6 @@ app.get("/", (req, res) => {
 
 app.get("/get-oauth-link", async (req, res) => {
   const state = uuidv4();
-  // logger.info(`@CodeTropolis: state ${state}`)
   req.session.state = state
   const args = new URLSearchParams({
     state,
@@ -74,11 +63,10 @@ app.get("/authorize-oauth", async (req, res) => {
   }).then(
     (response) => {
       var connected_account_id = response.stripe_user_id;
-    //  console.log(`@CodeTropolis: connected_account_id ${connected_account_id}`);
       saveAccountId(connected_account_id);
 
       // Render some HTML or redirect to a different page.
-      // return res.redirect(301, '/success.html')
+      return res.redirect(301, '/success.html')
     },
     (err) => {
       if (err.type === 'StripeInvalidGrantError') {
